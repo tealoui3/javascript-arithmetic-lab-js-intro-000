afda7d86732b41790cdb82227a13e3faaa0e513f
Tiffany Louie <tealouie@gmail.com>
2019-03-03 03:57:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var a = 20
+var b = 5
+function add() {
+	a + b;
+}
+function subtract() {
+	b-a;
+}
+function add() {
+	a + b;
+}
+function multiply() {
+	a * b;
+}
+function divide() {
+	a / b;
+}
+function inc() {
+	a++;
+}
+function dec() {
+	a--;
+}
+function makeInt() {
+	parseInt(a, 10);
+}
